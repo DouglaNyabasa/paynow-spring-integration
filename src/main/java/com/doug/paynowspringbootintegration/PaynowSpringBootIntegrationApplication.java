b6937f0d61c454682cbc2f718369adef81f8ad61
douglanyabasa <douglasnyabasa400@gmail.com>
2026-04-13 16:09:33 +0200

    initial commit

---
+++ src/main/java/com/doug/paynowspringbootintegration/PaynowSpringBootIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.doug.paynowspringbootintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaynowSpringBootIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaynowSpringBootIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doug/paynowspringbootintegration/PaynowSpringBootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doug.paynowspringbootintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaynowSpringBootIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
